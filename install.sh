9cfe74ad2fc44b8e47d276bbd628f8bcf296aa1c
aderama2711 <ade.ramadha@gmail.com>
2022-07-19 11:05:15 +0700

    add script

---
+++ install.sh
@@ -0,0 +1,43 @@
+sudo apt install build-essential pkg-config python3-minimal libboost-all-dev libssl-dev libsqlite3-dev -y
+git clone https://github.com/named-data/ndn-cxx
+cd ndn-cxx
+./waf configure
+sudo ./waf install
+cd ..
+echo "ndn-cxx installed!"
+
+sudo apt install libpcap-dev libsystemd-dev -y
+git clone --recursive https://github.com/named-data/NFD.git
+cd NFD
+./waf configure
+./waf
+sudo ./waf install
+cd ..
+echo "NFD Installed"
+
+git clone https://github.com/named-data/PSync
+cd PSync
+./waf configure
+./waf
+sudo ./waf install
+cd ..
+echo "PSync Installed"
+
+git clone https://github.com/named-data/NLSR
+cd NLSR
+./waf configure
+./waf
+sudo ./waf install
+cd ..
+echo "NLSR Installed"
+
+git clone https://github.com/aderama2711/ndn-traffic-generator_modified
+cd ndn-traffic-generator_modified
+chmod +x waf
+./waf configure
+./waf
+sudo ./waf install
+cd ..
+echo "TrafficGen Installed"
+sudo ldconfig
+echo "DONE!"
