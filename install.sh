24e797004a994f6180a66bf8462beb640099677b
aderama2711 <ade.ramadha@gmail.com>
2023-01-03 03:34:02 +0700

    Fixed version to ndn-cxx-0.8.0 and NFD-22.02

--- install.sh
+++ install.sh
@@ -1,17 +1,23 @@
 sudo apt install build-essential pkg-config python3-minimal libboost-all-dev libssl-dev libsqlite3-dev libpcap-dev libsystemd-dev
 git clone https://github.com/named-data/ndn-cxx
 cd ndn-cxx
-./waf configure
-sudo ./waf install
-cd ..
-echo "ndn-cxx installed!"
-
-git clone --recursive https://github.com/named-data/NFD.git
-cd NFD
+git branch ndn-cxx-0.8.0
 ./waf configure
 ./waf
 sudo ./waf install
 cd ..
+sudo ldconfig
+echo "ndn-cxx installed!"
+
+sudo apt install libpcap-dev libsystemd-dev
+git clone --recursive https://github.com/named-data/NFD.git
+cd NFD
+git branch NFD-22.02
+./waf configure
+./waf
+sudo ./waf install
+cd ..
+sudo ldconfig
 echo "NFD Installed"
 
 git clone https://github.com/named-data/PSync
@@ -20,6 +26,7 @@ cd PSync
 ./waf
 sudo ./waf install
 cd ..
+sudo ldconfig
 echo "PSync Installed"
 
 git clone https://github.com/named-data/NLSR
@@ -28,11 +35,11 @@ cd NLSR
 ./waf
 sudo ./waf install
 cd ..
+sudo ldconfig
 echo "NLSR Installed"
 
-git clone https://github.com/aderama2711/ndn-traffic-generator_modified
-cd ndn-traffic-generator_modified
-chmod +x waf
+git clone https://github.com/aderama2711/ndn-traffic-generator
+cd ndn-traffic-generator
 ./waf configure
 ./waf
 sudo ./waf install
